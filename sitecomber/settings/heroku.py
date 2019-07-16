338b36d83427e3b451798d72b2d44e92d651e48c
Nina Pavlich <nina@ninalp.com>
2019-07-16 12:49:27 -0700

    working on one-click heroku deployment

--- sitecomber/settings/heroku.py
+++ sitecomber/settings/heroku.py
@@ -11,6 +11,7 @@ CSRF_TRUSTED_ORIGINS = (
     env('APP_HOST_NAME')
 )
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
