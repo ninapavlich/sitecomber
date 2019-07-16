6d06fda7dd384672b25bf4abf04359ac174b5dd0
Nina Pavlich <nina@ninalp.com>
2019-07-16 12:21:29 -0700

    working on one-click heroku deployment

---
+++ sitecomber/settings/heroku.py
@@ -0,0 +1,12 @@
+from sitecomber.settings.base import *
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+ALLOWED_HOSTS = [env('APP_HOST_NAME')]
+CORS_ORIGIN_WHITELIST = (
+    env('APP_HOST_NAME')
+)
+CSRF_TRUSTED_ORIGINS = (
+    env('APP_HOST_NAME')
+)
--- sitecomber/settings/local.py
+++ sitecomber/settings/local.py
@@ -3,6 +3,14 @@ from sitecomber.settings.base import *
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 INTERNAL_IPS = ['127.0.0.1', 'localhost']
 
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8080',
+    '127.0.0.1:8000'
+)
+CSRF_TRUSTED_ORIGINS = (
+    'localhost:8080',
+    '127.0.0.1:8000'
+)
 
 LOGGING['handlers']['console']['level'] = 'INFO'
 LOGGING['handlers']['file']['level'] = 'INFO'
