aafd4a52f1ae0da527346163e9af89f7c1327bbf
Nina Pavlich <nina@ninalp.com>
2019-07-16 14:10:43 -0700

    working on serving static files

--- sitecomber/settings/base.py
+++ sitecomber/settings/base.py
@@ -187,13 +187,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_LOCATION = 'static'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'sitecomber', 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'sitecomber', 'static'),
+# )
 
 # Uploads
 MEDIA_ROOT = env("MEDIA_ROOT", default=os.path.join(
