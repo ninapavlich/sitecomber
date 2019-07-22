20f84a3ee9caa1e4b33ccb4d1d1c00c677fa390a
Nina Pavlich <nina@ninalp.com>
2019-07-21 22:37:36 -0700

    add spellcheck error highlighting to article preview

--- sitecomber/apps/results/templatetags/results_tags.py
+++ sitecomber/apps/results/templatetags/results_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -6,3 +8,11 @@ register = template.Library()
 @register.simple_tag
 def get_test_result_by_type(value, arg):
     return value.get_test_result_by_type(arg)
+
+
+@register.filter(needs_autoescape=True)
+@stringfilter
+def highlight_spelling_errors(value, misspellings, autoescape=True):
+    for word in misspellings:
+        value = value.replace(word, "<span class='bg-danger text-light'>%s</span>" % word)
+    return mark_safe(value)
