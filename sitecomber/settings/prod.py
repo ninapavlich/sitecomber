62c2c13dd9c0cac1cedf42406e6b22586bedfd29
Nina Pavlich <nina@ninalp.com>
2019-08-31 23:54:21 -0700

    working on deploying to sitecomber

--- sitecomber/settings/prod.py
+++ sitecomber/settings/prod.py
@@ -1,7 +1,7 @@
 from sitecomber.settings.base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['35.160.26.81']
+ALLOWED_HOSTS = ['35.160.26.81', 'sitecomber.ninalp.com']
 
 
 STATIC_ROOT = '/srv/sitecomber/static/'
