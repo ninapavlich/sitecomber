a9af7ac15f7dc0135e0aa0684d4312b2fc979442
Nina Pavlich <nina@ninalp.com>
2019-09-01 00:01:50 -0700

    working on deploying to sitecomber

--- sitecomber/settings/__init__.py
+++ sitecomber/settings/__init__.py
@@ -3,7 +3,14 @@ import environ
 root = environ.Path(__file__) - 3  # three folder back (/a/b/c/ - 3 = /)
 env = environ.Env()  # set default values and casting
 environ.Env.read_env(env_file=root('.env'))
-print(root('.env'))
+
+env_file = root('.env')
+print("READ FROM %s" % (env_file))
+env_file_handler = open(env_file, 'r')
+env_contents = env_file_handler.read()
+print(env_contents)
+env_file_handler.close()
+
 SECRET_KEY = env('SECRET_KEY')
 
 ENVIRONMENT = env('ENVIRONMENT', default='local')
