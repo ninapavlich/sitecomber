c5913d923d2bc3064325621a0e663b9f466316f5
Nina Pavlich <nina@ninalp.com>
2019-09-01 00:07:39 -0700

    remove debug bits

--- sitecomber/settings/prod.py
+++ sitecomber/settings/prod.py
@@ -1,7 +1,13 @@
 from sitecomber.settings.base import *
 
 DEBUG = False
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
 ALLOWED_HOSTS = ['35.160.26.81', 'sitecomber.ninalp.com']
+CORS_ORIGIN_WHITELIST = ALLOWED_HOSTS
+CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
 
 
 STATIC_ROOT = '/srv/sitecomber/static/'
