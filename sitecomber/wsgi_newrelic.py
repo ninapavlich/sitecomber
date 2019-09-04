f105491fdb4ac5b2720481bba8389f73d65bbc47
Nina Pavlich <nina@ninalp.com>
2019-09-03 23:32:09 -0700

    debugging newrelic

--- sitecomber/wsgi_newrelic.py
+++ sitecomber/wsgi_newrelic.py
@@ -11,16 +11,9 @@ environ.Env.read_env(env_file=root('.env'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sitecomber.settings")
 
-
-NEW_RELIC_LICENSE_KEY = env('NEW_RELIC_LICENSE_KEY', default=None)
-
-print("NEW_RELIC_LICENSE_KEY: %s" % (NEW_RELIC_LICENSE_KEY))
-if NEW_RELIC_LICENSE_KEY:
-    print("use new relic!")
-    NEW_RELIC_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'newrelic.ini'))
-    NEW_RELIC_FILE.replace("{{license_key}}", NEW_RELIC_LICENSE_KEY)
-    print(NEW_RELIC_FILE)
-    newrelic.agent.initialize(NEW_RELIC_FILE)
+NEW_RELIC_CONFIG_FILE = env('NEW_RELIC_CONFIG_FILE', default=None)
+if NEW_RELIC_CONFIG_FILE:
+    newrelic.agent.initialize(NEW_RELIC_CONFIG_FILE)
 
     application = get_wsgi_application()
     application = newrelic.agent.wsgi_application()(application)
