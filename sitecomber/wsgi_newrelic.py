10750f10425da760e9582f017c32e439e097b8b1
Nina Pavlich <nina@ninalp.com>
2019-09-03 23:26:28 -0700

    setting up new relic for ninalp installation

--- sitecomber/wsgi_newrelic.py
+++ sitecomber/wsgi_newrelic.py
@@ -9,18 +9,17 @@ root = environ.Path(__file__) - 2
 env = environ.Env()
 environ.Env.read_env(env_file=root('.env'))
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sitecomber.settings")
+
 
 NEW_RELIC_LICENSE_KEY = env('NEW_RELIC_LICENSE_KEY', default=None)
 
 if NEW_RELIC_LICENSE_KEY:
     NEW_RELIC_FILE = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'newrelic.ini'))
     NEW_RELIC_FILE.replace("{{license_key}}", NEW_RELIC_LICENSE_KEY)
+    print(NEW_RELIC_FILE)
     newrelic.agent.initialize(NEW_RELIC_FILE)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sitecomber.settings")
-
-
-if NEW_RELIC_LICENSE_KEY:
     application = get_wsgi_application()
     application = newrelic.agent.wsgi_application()(application)
 
