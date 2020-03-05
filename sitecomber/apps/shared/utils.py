59739367cf834bb28c341b3fe26568801460f2a3
Nina Pavlich <nina.pavlich@nytimes.com>
2020-03-05 15:50:52 -0800

    adjust title parser to make sure we are in header tag before parsing title

--- sitecomber/apps/shared/utils.py
+++ sitecomber/apps/shared/utils.py
@@ -139,9 +139,12 @@ class TitleParser(HTMLParser):
         HTMLParser.__init__(self)
         self.match = False
         self.title = ''
+        self.inHead = False
 
     def handle_starttag(self, tag, attributes):
-        self.match = True if tag == 'title' else False
+        if tag == 'head':
+            self.inHead = True
+        self.match = True if (self.inHead and tag == 'title') else False
 
     def handle_data(self, data):
         if self.match:
